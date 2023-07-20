bb5226bb34325c781607ac25a8b58405908ec42e
Marcel Schmidt <marcelschmidt1989@live.de>
2023-07-20 17:56:03 +0200

    deploy

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,5 @@
 import {defineConfig} from 'vite';
-import react from '@vitejs/plugin-react';
 
 export default defineConfig({
-    base: '/results-summary-component-main',
-    plugins: [react()]
+    base: '/results-summary-component-main/'
 });
